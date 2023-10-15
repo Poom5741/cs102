853a2fdbd7e050f1a96b0b3b508369011f3d996c
poom <root@Pooms-MacBook-Pro.local>
2023-10-15 12:18:18 +0700

    update magicSquare

--- magicSqure-simple.c
+++ magicSqure-simple.c
@@ -14,23 +14,141 @@ int main() {
 
     printf("Enter your square:\n");
 
-    // Input the square values
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &square[i][j]);
+    if (n == 3) {
+        printf("Enter 3x3 square:\n");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 4) {
+        printf("Enter 4x4 square:\n");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 5) {
+        printf("Enter 5x5 square:\n");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 6) {
+        printf("Enter 6x6 square:\n");
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 7) {
+        printf("Enter 7x7 square:\n");
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 8) {
+        printf("Enter 8x8 square:\n");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 9) {
+        printf("Enter 9x9 square:\n");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 10) {
+        printf("Enter 10x10 square:\n");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 11) {
+        printf("Enter 11x11 square:\n");
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 12) {
+        printf("Enter 12x12 square:\n");
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 13) {
+        printf("Enter 13x13 square:\n");
+        for (int i = 0; i < 13; i++) {
+            for (int j = 0; j < 13; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 14) {
+        printf("Enter 14x14 square:\n");
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 14; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 15) {
+        printf("Enter 15x15 square:\n");
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 16) {
+        printf("Enter 16x16 square:\n");
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 17) {
+        printf("Enter 17x17 square:\n");
+        for (int i = 0; i < 17; i++) {
+            for (int j = 0; j < 17; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 18) {
+        printf("Enter 18x18 square:\n");
+        for (int i = 0; i < 18; i++) {
+            for (int j = 0; j < 18; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 19) {
+        printf("Enter 19x19 square:\n");
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 19; j++) {
+                scanf("%d", &square[i][j]);
+            }
+        }
+    } else if (n == 20) {
+        printf("Enter 20x20 square:\n");
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                scanf("%d", &square[i][j]);
+            }
         }
     }
 
-    // Calculate the sum of the first row for comparison
+    // Rest of the code remains the same
     int sum = 0;
     for (int j = 0; j < n; j++) {
         sum += square[0][j];
     }
-
-    // Check if it's a magic square and calculate the magic constant
-    int isMagic = 1;  // Assume it's a magic square
-    int maxSum = 0;   // Maximum sum for non-magic squares
-
+    int isMagic = 1;
+    int maxSum = 0;
     for (int i = 0; i < n; i++) {
         int rowSum = 0;
         int colSum = 0;
