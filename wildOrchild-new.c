f309f034c78c3861d3deaee706f8ee81930b3a70
poom <root@Pooms-MacBook-Pro.local>
2023-10-30 15:20:30 +0700

    new fuking version of wildOrchild

--- wildOrchild-new.c
+++ wildOrchild-new.c
@@ -22,12 +22,10 @@ int main()
         int b = array1[1][i];
         if (a >= '0' && a <= '9')
         {
-            array2[(a - '0') - 1] += 1; // Convert character to integer and update the count
             array3[(a - '0') - 1][b - 'A'] += 1;
         }
         else if (a >= 'A')
         {
-            array2[(a - 'A') + 9] += 1; // Update the count for characters A to L
             array3[(a - 'A') + 9][b - 'A'] += 1;
         }
     }
@@ -58,28 +56,5 @@ int main()
         printf("%d\n",array4[i]);
     }
     printf("%d",most);
-    
-
-    // // Print the separated rows
-    // for (int i = 0; i < 16; i++)
-    // {
-    //     printf("%c %c\n", array1[0][i], array1[1][i]);
-    // }
-
-    // // Print the counts from array2
-    // for (int i = 0; i < 12; i++)
-    // {
-    //     printf("this is array 2 : %d\n", array2[i]);
-    // }
-
-    // for (int i = 0; i < 12; i++)
-    // {
-    //     for (int j = 0; j < 26; j++)
-    //     {
-    //         printf("%d ", array3[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
     return 0;
 }
